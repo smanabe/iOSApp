346da2252b1f30f37e5561146264604dfb63bf3b
satoshi manabe <smanabe@satoshi-no-MacBook-Pro.local>
2014-06-24 11:02:15 +0900

    add_apps
    add_apps

---
+++ MySecondApp/MySecondApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MySecondApp
+//
+//  Created by satoshi manabe on 2014/06/23.
+//  Copyright (c) 2014年 HomeWork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MySecondApp/MySecondApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MySecondApp
+//
+//  Created by satoshi manabe on 2014/06/23.
+//  Copyright (c) 2014年 HomeWork. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
